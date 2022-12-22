6d67df3df791bc3d6394323f821aedc2df62cb7b
Samuel Ohiri <therealsammy18x@gmail.com>
2022-12-22 20:20:40 +0100

    Objects for alx buddies

---
+++ alx_class.py
@@ -0,0 +1,50 @@
+class Car:
+    def __init__(self, make, model):
+        self.make = make
+        self.model = model
+
+    def start(self):
+        print("car started")
+
+    def stop(self):
+        print("car stopped")
+
+    def move(self):
+        print("car is moving")
+
+
+mercedes_e350 = Car("mercedes", "e350")
+mercedes_c230 = Car("mercedes", "c230")
+# print(f"This is a {mercedes_e350.make} of model {mercedes_e350.model}")
+# print(f"This is a {mercedes_c230.make} of model {mercedes_c230.model}")
+
+# mercedes_e350.start()
+
+
+class Human:
+    def __init__(self, name=str, age=int, race=str, eye_color=str, quirk=str):
+        self.name = name
+        self.age = age
+        self.race = race
+        self.eye_color = eye_color
+        self.quirk = quirk
+
+    def talk(self):
+        print(f"{self.name} is talking to you")
+
+    def sleep(self):
+        print(f"{self.name} is sleeping")
+
+    def birthday(self):
+        print(f"Hurray!!! It's {self.name}'s {self.age + 1}'th birthday.")
+
+
+sam = Human("Sam", 27, "Black", "Brown", "Talks to himself")
+sam.talk()
+sam.birthday()
+
+jules = Human("Jules", 23, "African", "Brown", "Doesn't remember the names of movies she has seen ")
+print(jules.quirk)
+
+
+
