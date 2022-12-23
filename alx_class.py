f81d277126981740ed6b5344e8e49d5986d31a5e
Samuel Ohiri <therealsammy18x@gmail.com>
2022-12-23 20:10:46 +0100

    rock, paper, scissors

--- alx_class.py
+++ alx_class.py
@@ -4,7 +4,7 @@ class Car:
         self.model = model
 
     def start(self):
-        print("car started")
+        return "car started"
 
     def stop(self):
         print("car stopped")
@@ -18,7 +18,8 @@ mercedes_c230 = Car("mercedes", "c230")
 # print(f"This is a {mercedes_e350.make} of model {mercedes_e350.model}")
 # print(f"This is a {mercedes_c230.make} of model {mercedes_c230.model}")
 
-# mercedes_e350.start()
+start_car = mercedes_e350.start()
+print(start_car)
 
 
 class Human:
@@ -39,12 +40,25 @@ class Human:
         print(f"Hurray!!! It's {self.name}'s {self.age + 1}'th birthday.")
 
 
-sam = Human("Sam", 27, "Black", "Brown", "Talks to himself")
-sam.talk()
-sam.birthday()
+# sam = Human("Sam", 27, "Black", "Brown", "Talks to himself")
+# sam.talk()
+# sam.birthday()
+#
+# jules = Human("Jules", 23, "African", "Brown", "Doesn't remember the names of movies she has seen ")
+# print(jules.quirk)
 
-jules = Human("Jules", 23, "African", "Brown", "Doesn't remember the names of movies she has seen ")
-print(jules.quirk)
+def me(f):
+    def f():
+        return "hi"
+
+    return f
+
+
+def you():
+    pass
+
+print(me)
+print(me(you)())
 
 
 
